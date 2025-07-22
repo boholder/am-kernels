42bff258770f3ad44846793191dc6aae42526b5a
boholder <bottleholder@anche.no>
2025-07-22 11:18:22 +0800

    feat: 测试 klib memset

---
+++ tests/klib-tests/include/trap.h
@@ -0,0 +1,40 @@
+#ifndef __TRAP_H__
+#define __TRAP_H__
+
+#include "am.h"
+#include "klib-macros.h"
+#include "klib.h"
+#include <stdint.h>
+
+__attribute__((noinline))
+void check(bool cond) {
+  if (!cond) halt(1);
+}
+
+#define N 32
+uint8_t data[N];
+
+void reset() {
+  int i;
+  for (i = 0; i < N; i ++) {
+    data[i] = i + 1;
+  }
+}
+
+// 检查[l,r)区间中的值是否依次为val, val + 1, val + 2...
+void check_seq(int l, int r, int val) {
+  int i;
+  for (i = l; i < r; i ++) {
+    assert(data[i] == val + i - l);
+  }
+}
+
+// 检查[l,r)区间中的值是否均为val
+void check_eq(int l, int r, int val) {
+  int i;
+  for (i = l; i < r; i ++) {
+    assert(data[i] == val);
+  }
+}
+
+#endif
