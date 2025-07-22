c6dca0c6281f27527bdd892957e8ed6fdcb388d1
boholder <bottleholder@anche.no>
2025-07-22 13:57:48 +0800

    test: klib memcpy

---
+++ tests/klib-tests/tests/memcpy.c
@@ -0,0 +1,20 @@
+#include "../include/trap.h"
+
+int main()
+{
+  uint8_t src_arr[N];
+  for (int i = 0; i < N; i ++) {
+    src_arr[i] = i + 40;
+  }
+
+  int l, r;
+  for (l = 0; l < N; l++) {
+    for (r = l + 1; r <= N; r++) {
+      reset();
+      memcpy(data + l, src_arr, r - l);
+      check_seq(0, l, 1);
+      check_seq(l, r, 40);
+      check_seq(r, N, r + 1);
+    }
+  }
+}
